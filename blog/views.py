3982044ab3c4c0cfb1758f65678e3c64151f5856
lisn0 <lisnoxvel@gmail.com>
2018-11-22 01:24:48 +0100

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
+from typing import Dict, List
+
 from django.shortcuts import render
 
-# Create your views here.
+
+posts: List[Dict[str, str]] = [
+    {
+        'author': 'Bilel',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'Nov 22, 2018',
+    },
+
+    {
+        'author': 'zennd',
+        'title': 'Blog post 1',
+        'content': 'Second post content',
+        'date_posted': 'Nov 23, 2018',
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
