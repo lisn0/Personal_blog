ce7b6f23f73b932e77b37654b13a7229395a1ae7
lisn0 <lisnoxvel@gmail.com>
2018-11-22 23:40:18 +0100

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,11 @@
 from typing import Dict, List
-
+from .models import Post
 from django.shortcuts import render
-
-
-posts: List[Dict[str, str]] = [
-    {
-        'author': 'Bilel',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'Nov 22, 2018',
-    },
-
-    {
-        'author': 'zennd',
-        'title': 'Blog post 1',
-        'content': 'Second post content',
-        'date_posted': 'Nov 23, 2018',
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
